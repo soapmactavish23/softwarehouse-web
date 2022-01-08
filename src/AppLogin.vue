fb80f8beab8fc79861ec36cd6483a1368573c852
soapmactavish23 <ricknogueira1231@gmail.com>
2022-01-08 15:44:02 -0300

    AppLOGIN

--- src/AppLogin.vue
+++ src/AppLogin.vue
@@ -1,12 +1,19 @@
 <template>
   <Toast />
   <!-- INICIO FORM LOGIN-->
-  <div class="grid fullScreen">
-    <div class="col-12 lg:col-3 xl:col-3 card">
-      <div class="shadow-4">
+  <div class="grid full">
+    <div class="col-12 xl:col-3 card">
+      <div class="shadow-4 fullScreen">
         <div class="text-center p-4">
-          <img src="../public/images/logo-login.svg" alt="logo" style="width: 70%;" />
+          <div class="p-d-flex p-ai-center p-jc-center">
+            <img
+              src="../public/images/logo-login.svg"
+              alt="logo"
+              class="logo"
+            />
+          </div>
         </div>
+        <div class="layout-main"></div>
         <div class="card p-fluid">
           <form @submit.prevent="logar()">
             <div class="field">
@@ -51,8 +58,6 @@
               />
             </div>
           </form>
-        </div>
-        <div class="mt-4">
           <div class="layout-footer">
             <span class="footer-text mb-3"
               ><img src="../public/images/logoFooter.svg" width="135"
@@ -61,25 +66,35 @@
         </div>
       </div>
     </div>
-    <div class="md:col-8 lg:col-9 xl:col-9 painelDiv">
-      <div>
-        <img src="../public/images/wallpapper.png" class="img-fluid" />
+    <div class="xl:col-9 card">
+      <div class="painel">
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  components: {},
+};
 </script>
-
 <style lang="scss" scoped>
+.painel {
+  height: 100%;
+  background-image: url("../public/images/wallpapper.png");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+.full {
+  margin: 0px auto;
+}
 .layout-footer {
   background-color: #dddddd;
   box-shadow: 0px 6px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  margin-top: auto;
 }
 .fullScreen {
-  height: 99vh;
+  height: 100%;
   overflow: hidden;
   margin: 0px auto;
 }
@@ -87,9 +102,31 @@ export default {};
   min-width: 100%;
   height: auto;
 }
+.logo {
+  width: 100%;
+}
+@media screen and (max-width: 1200px) {
+  .painelDiv {
+    display: none !important;
+  }
+  .logo {
+    width: 35%;
+  }
+}
 @media screen and (max-width: 992px) {
   .painelDiv {
     display: none !important;
+  }
+  .logo {
+    width: 40%;
+  }
+}
+@media screen and (max-width: 768px) {
+  .painelDiv {
+    display: none !important;
+  }
+  .logo {
+    width: 60%;
   }
 }
 </style>
